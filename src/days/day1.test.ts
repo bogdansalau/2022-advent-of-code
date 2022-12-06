e86c20e27f513f580a0d685832f53369659aba07
Bogdan Salau <bogdan.salau@cutr.ai>
2022-12-06 14:27:24 +0200

    solve day 1

--- src/days/day1.test.ts
+++ src/days/day1.test.ts
@@ -1,6 +1,19 @@
 import Day from './day1';
 import { dayRunner } from './test-runner';
 
-const example = `input`;
+const example = `1000
+2000
+3000
 
-dayRunner(Day, example, 1, 2);
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+dayRunner(Day, example, 24000, 45000);
--- src/days/day1.ts
+++ src/days/day1.ts
@@ -1,11 +1,21 @@
 import { BaseDay } from '../day';
 
 export class Day extends BaseDay<number[], number, number> {
-  parse(input: string): number[] {}
+  parse(input: string): number[] {
+    return input
+      .split('\n\n')
+      .map((it) => it.split('\n').map((it) => parseInt(it)))
+      .map((it) => it.reduce((x, y) => x + y));
+  }
 
-  async partOne(): Promise<number> {}
+  async partOne(): Promise<number> {
+    return this.input.reduce((a, b) => (b > a ? b : a), 0);
+  }
 
-  async partTwo(): Promise<number> {}
+  async partTwo(): Promise<number> {
+    const r = this.input.sort((a, b) => b - a);
+    return r[0] + r[1] + r[2];
+  }
 }
 
 export default Day;
